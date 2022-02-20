24d94bc98ed6d6198000bee9674b6ad941d365bd
Shreya Sharma <shreyasharmans@gmail.com>
2022-02-20 17:51:41 +0530

    Portfolio

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -10,12 +10,11 @@ const Product = ({imgg,linkk,link11,title,project,project1}) => {
 			<div className="face face1">
 				<div className="content">
 				<img src={imgg} alt="" className="p-img"/>
-        {/* <img src={Award} alt="" className="p-img"/> */}
 					<h3>{title}</h3>
 				</div>
 			</div>
 			<div className="face face2">
-				<div className="content"><br></br>
+				<div className="content"><br></br><br></br><br></br>
 					<p>Click Below</p>
 					<a href={linkk}><h4>{project}</h4></a>
           <a href={link11}> <h4 className={(project1==="") ? 'hdn' : 'vsbl'}>{project1}</h4></a>
